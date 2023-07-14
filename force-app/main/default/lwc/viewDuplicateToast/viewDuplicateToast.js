796cf6069894d450d7e55d63e343e77ec021ea0d
lulu <chia-ju.lin@diva-e.com>
2023-07-14 19:32:04 +0200

    toast with link

--- force-app/main/default/lwc/viewDuplicateToast/viewDuplicateToast.js
+++ force-app/main/default/lwc/viewDuplicateToast/viewDuplicateToast.js
@@ -1,25 +1,29 @@
 import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+import './viewDuplicateToast.css';
 
-export default class MyLWCComponent extends LightningElement {
+
+export default class MyLWCComponent extends NavigationMixin(LightningElement) {
     @api recordId;
 
-    handleButtonClick() {
-        if (this.recordId) {
-            // Construct the VF page URL with the current record ID as a parameter
-            const vfPageUrl = `/apex/mergeDuplicateSelection?masterAccountId=${this.recordId}`;
+    get vfPageUrl() {
+        return `/apex/mergeDuplicateSelection?masterAccountId=${this.recordId}`;
+    }
 
-            // Show a toast message with a link
-            const toastMessage = `Click <a href="${vfPageUrl}">here</a> to go to the VF page.`;
-            const toastEvent = new ShowToastEvent({
-                title: 'Toast Message',
-                message: toastMessage,
-                variant: 'info',
-                mode: 'sticky' // Use 'sticky' mode to allow users to click the link in the toast message
-            });
-            this.dispatchEvent(toastEvent);
-        }
+    handleButtonClick() {
+        const event = new ShowToastEvent({
+            title: 'Potential duplicates',
+            message: '{0}',
+            messageData: [
+                {
+                    url: this.vfPageUrl,
+                    label: 'View Duplicates',
+                },
+            ],
+            variant: 'info',
+        });
+
+        this.dispatchEvent(event);
     }
 }
-
-
