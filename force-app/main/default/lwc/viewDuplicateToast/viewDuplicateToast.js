3f92b7271ebc58686dd874b85da5a09292d9e824
lulu <chia-ju.lin@diva-e.com>
2023-07-15 10:26:06 +0200

    cout displayed

--- force-app/main/default/lwc/viewDuplicateToast/viewDuplicateToast.js
+++ force-app/main/default/lwc/viewDuplicateToast/viewDuplicateToast.js
@@ -1,7 +1,9 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
 import './viewDuplicateToast.css';
+import getDuplicateAccountsCount from '@salesforce/apex/AccountMergeHelper.getDuplicateAccountsCount';
+
 
 
 export default class MyLWCComponent extends NavigationMixin(LightningElement) {
@@ -10,6 +12,19 @@ export default class MyLWCComponent extends NavigationMixin(LightningElement) {
     get vfPageUrl() {
         return `/apex/mergeDuplicateSelection?masterAccountId=${this.recordId}`;
     }
+    duplicateAccountsCount = 0;
+
+    @wire(getDuplicateAccountsCount, { currentAccountId: '$recordId' })
+    wiredDuplicateAccountsCount({ error, data }) {
+        console.log('*******Record ID:', this.recordId); // Check the value of recordId
+        if (data) {
+            this.duplicateAccountsCount = data;
+            console.log('***********Duplicate Accounts Count:', this.duplicateAccountsCount); // Check the count value
+        } else if (error) {
+            console.error('********error: ', error);
+        }
+    }
+    
 
     handleButtonClick() {
         const event = new ShowToastEvent({
